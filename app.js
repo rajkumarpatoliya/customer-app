9a23adc74e65000c08112d571b245146edc7bcb9
w4721x <patoliyaraj@gmail.com>
2018-09-25 12:27:17 +0530

    Express Validation practice 1

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var expressValidator = require('express-validator');
 
 var app = express();
 var users = [
@@ -38,6 +39,30 @@ app.use(bodyParser.urlencoded({extended: false}));
 // Set static path
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Global variables
+
+app.use(function(req, res, next) {
+    res.locals.errors = null;
+    next();
+});
+
+// Express-validation Middleware-option errorFormatter
+app.use(expressValidator({
+    errorFormatter: function(param, msg, value) {
+        var namespace = param.split('.'),
+        root = namespace.shift(),
+        formParam = root;
+
+        while(namespace.length) {
+           formParam += '['+ namespace.shift() +']' 
+        }
+        return {
+            param: formParam,
+            msg: msg,
+            value: value
+        };
+    }
+}));
 app.get('/', function(req, res) {
     res.render('index', {
         title:'Customers',
@@ -45,6 +70,27 @@ app.get('/', function(req, res) {
     });
 });
 
+app.post('/users/add', function(req, res) {
+    req.checkBody('firstname', 'First Name is Required').notEmpty();
+    req.checkBody('lastname', 'Last Name is Required').notEmpty();
+    req.checkBody('email', 'Email is Required').notEmpty();
+    var errors =req.validationErrors(); 
+    if(errors) {
+        res.render('index', {
+            title:'Customers',
+            users: users, 
+            errors: errors
+        });
+    } else {
+        let newCustomer = {
+            firstname: req.body.firstname,
+            lastname: req.body.lastname,
+            email: req.body.email
+        }
+        console.log('Registration Successful...');
+    }
+    
+});
 
 /*var logger = function(req, res, next){
     console.log("Logging...");
